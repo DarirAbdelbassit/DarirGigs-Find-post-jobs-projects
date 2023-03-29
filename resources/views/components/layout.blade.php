3afa98ee81cc866b4432294f32ab2fd1e9a9d36d
DarirAbdelbassit <abdelbassitdarir@gmail.com>
2023-03-29 20:43:30 +0000

    done with alert of listing is add

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
+        <script src="//unpkg.com/alpinejs" defer></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
             tailwind.config = {
@@ -104,7 +105,7 @@
     <footer
     class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
 >
-    <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+    <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
     <a
         href="/listings/create"
