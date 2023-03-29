53cc8028df9da36507b127ee646d39624310808b
DarirAbdelbassit <abdelbassitdarir@gmail.com>
2023-03-29 02:10:52 +0000

    add components and views and create tables

---
+++ resources/views/components/all-cards.blade.php
@@ -0,0 +1,20 @@
+ @props(['item'])
+ <x-card>
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src="/images/no-image.png"
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="/listings/{{$item->id}}">{{$item->title}}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{{$item->company}}</div>
+            <x-tag :tags="$item->tags" />
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i> {{$item->location}}
+            </div>
+        </div>
+    </div>
+ </x-card>
---
+++ resources/views/listings.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+@section('content')
+    @if (!count($listings)==0)
+    <div
+    class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
+>
+    @foreach ($listings as $item)
+    <x-all-cards :item="$item" />
+    @endforeach
+</div>
+    @else
+    <h3>no Listing</h3>
+    @endif
+@endsection
