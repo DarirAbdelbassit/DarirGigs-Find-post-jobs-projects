e7ebf7784727f60b513a4a92ff6414a9f1b1c3dc
DarirAbdelbassit <abdelbassitdarir@gmail.com>
2023-03-30 15:57:37 +0000

    make hero and search as partials and fix ther display

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
+    @if (!Auth::check())
+    @include('partials.hero')
+    @endif
+    @include('partials.search')
     @if (!count($listings)==0)
     <div
     class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
